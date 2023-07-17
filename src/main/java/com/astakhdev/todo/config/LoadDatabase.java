50c1fc2cbcd3e95a1925eceea1f516a981a9fb37
Shikari88 <astakhin.rsm@mail.ru>
2023-07-17 17:37:18 +0300

    add class TodoController and interface TodoRepository. is set to connect to BD

--- src/main/java/com/astakhdev/todo/config/LoadDatabase.java
+++ src/main/java/com/astakhdev/todo/config/LoadDatabase.java
@@ -1,8 +1,23 @@
 package com.astakhdev.todo.config;
 
+import com.astakhdev.todo.model.ToDoLine;
+import com.astakhdev.todo.repository.TodoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.Date;
+
+@Configuration
 public class LoadDatabase {
     public static final Logger LOGGER = LoggerFactory.getLogger(LoadDatabase.class);
+    @Bean
+    CommandLineRunner initDatabase(TodoRepository todoRepository) {
+        return args -> {
+            LOGGER.info("Loading " + todoRepository.save(new ToDoLine("Купи продукты", 12, false, new Date())));
+            LOGGER.info("Loading " + todoRepository.save(new ToDoLine("Убери дом", 13, false, new Date())));
+        };
+    }
 }
